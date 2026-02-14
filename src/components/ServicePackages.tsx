306764cc94f2f3d330ea1fff4898a6ae5634b4b9
614restore <614restorellc@gmail.com>
2026-02-14 15:47:21 -0500

    Updated ServicePackages.tsx to ensure a proper default export.

---
+++ src/components/ServicePackages.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ServicePackages = () => {
+    return (
+        <div>
+            {/* Component content goes here */}
+            <h1>Service Packages</h1>
+        </div>
+    );
+};
+
+export default ServicePackages;
